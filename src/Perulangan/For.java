86ce998900070f7d53226cba83b87482ef5d4dcf
rizkaoktaviyani <rizkaoktaviyani10@gmail.com>
2020-04-26 21:38:48 +0800

    PerulanganWhile

---
+++ src/Perulangan/For.java
@@ -0,0 +1,18 @@
+package Perulangan;
+
+public class For {
+    public static void main(String[] args){
+        int isiGelas = 0;
+        int penuh = 225;
+
+        System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+        System.out.println("Isi Gelas Ketika Penuh : " + penuh + "ml");
+
+        for(isiGelas = 0 ; isiGelas <= penuh ; isiGelas++){
+            System.out.println("Sedang mengisi gelas..");
+            System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+        }
+
+        System.out.println("Finale: Isi Gelas Sekarang : " + isiGelas + "ml");
+    }
+}

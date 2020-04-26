812ef7d589c32e1c7e495f92d5ee83618889fbda
rizkaoktaviyani <rizkaoktaviyani10@gmail.com>
2020-04-27 00:25:40 +0800

    Tanaman

---
+++ src/Tugas/Array.java
@@ -0,0 +1,12 @@
+package Tugas;
+
+public class Array {
+    public static void main(String[] args){
+
+        String[] tanaman = {"Melati", "Mawar", "Kaktus", "Anggrek"};
+
+        for (int i = 0; i < tanaman.length; i++) {
+            System.out.println("Indeks ke-" + i + ": " + tanaman[i]);
+        }
+    }
+}
